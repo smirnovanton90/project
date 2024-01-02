55ea9ab0aa11b11d825a32a9fd971e0b0bef0a45
Anton Smirnov <smirnovanton90@gmail.com>
2024-01-02 14:53:37 +0300

    Added MLFlow code

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -1,7 +1,15 @@
 import requests
 import json
-import pandas as pd 
+import pandas as pd
+import os
+
+import mlflow
+from mlflow.tracking import MlflowClient
  
+os.environ["MLFLOW_REGISTRY_URI"] = "/home/sflow-admin/project/mlflow/"
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("get_data")
+
 # Необходимые справочники данных
 ids = []
 names = []
@@ -12,34 +20,38 @@ weights = []
 exp = []
 
 # Количество покемонов для запроса
-num = 100
+num = 5
 
-# собираем данные через API
-for id in range(num)[1:]:
-    uri = f"https://pokeapi.co/api/v2/pokemon/{id}/"
-    content = requests.get(uri).text
-    data = json.loads(content)
-    ids.append(data['id'])
-    names.append(data['name'])
-    main_types.append(data['types'][0]['type']['name'])
-    heights.append(data['height'])
-    weights.append(data['weight'])    
-    exp.append(data['base_experience'])
+with mlflow.start_run():
+    # собираем данные через API
+    for id in range(num)[1:]:
+        uri = f"https://pokeapi.co/api/v2/pokemon/{id}/"
+        content = requests.get(uri).text
+        data = json.loads(content)
+        ids.append(data['id'])
+        names.append(data['name'])
+        main_types.append(data['types'][0]['type']['name'])
+        heights.append(data['height'])
+        weights.append(data['weight'])    
+        exp.append(data['base_experience'])
+        
+        uri_2 = data['forms'][0]['url']
+        content_2 = requests.get(uri_2).text
+        data_2 = json.loads(content_2)
+        is_mega.append(data_2['is_mega'])
     
-    uri_2 = data['forms'][0]['url']
-    content_2 = requests.get(uri_2).text
-    data_2 = json.loads(content_2)
-    is_mega.append(data_2['is_mega'])
- 
-# Формируем датафрейм из собранных данных
-data = {'name': names,
-        'main_type': main_types,
-        'is_mega': is_mega,
-        'height': heights,
-        'weight': weights,     
-        'exp': exp}
-df = pd.DataFrame(data, 
-                  index = ids)
+    # Формируем датафрейм из собранных данных
+    data = {'name': names,
+            'main_type': main_types,
+            'is_mega': is_mega,
+            'height': heights,
+            'weight': weights,     
+            'exp': exp}
+    df = pd.DataFrame(data, 
+                    index = ids)
+    mlflow.log_artifact(local_path="/home/sflow-admin/project/scripts/get_data.py",
+                        artifact_path="get_data code")
+    mlflow.end_run()
 
 # Записываем в файл
 df.to_csv('/home/sflow-admin/project/datasets/data.csv')
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,7 +1,13 @@
 from sklearn.linear_model import LinearRegression
 import pickle
 import pandas as pd
+
+import mlflow
+from mlflow.tracking import MlflowClient
  
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("train_model")
+
 # Считываем из файла 
 df = pd.read_csv('/home/sflow-admin/project/datasets/data_train.csv')
 
@@ -11,6 +17,15 @@ Y_train = df['exp']
 
 # Обучаем модель линейной регрессии
 model = LinearRegression()
+
+with mlflow.start_run():
+    mlflow.sklearn.log_model(model,
+                             artifact_path="lr",
+                             registered_model_name="lr")
+    mlflow.log_artifact(local_path="/home/sflow-admin/project/scripts/train_model.py",
+                        artifact_path="train_model code")
+    mlflow.end_run()
+
 model.fit(X_train, Y_train)
 
 # Сохраняем получившуюся модель в файл
